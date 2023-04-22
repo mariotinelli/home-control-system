435c3690359f6bc499ddf69cff8412eb39d7254e
mariotinelli <maario_tinelli@hotmail.com>
2023-04-22 13:16:08 -0300

    back: feat - destroy goals and your tests

--- app/Http/Livewire/Goals/Destroy.php
+++ app/Http/Livewire/Goals/Destroy.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Livewire\Goals;
 
+use App\Models\Goal;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Destroy extends Component
 {
-    public function render()
+
+    public ?Goal $goal = null;
+
+    public function save(): void
+    {
+        $this->goal->forceDelete();
+
+        $this->emit('goal::deleted');
+    }
+
+    public function render(): View
     {
         return view('livewire.goals.destroy');
     }
--- tests/Feature/Livewire/Goals/DestroyTest.php
+++ tests/Feature/Livewire/Goals/DestroyTest.php
@@ -2,19 +2,40 @@
 
 namespace Tests\Feature\Livewire\Goals;
 
-use App\Http\Livewire\Goals\Destroy;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Livewire\Livewire;
-use Tests\TestCase;
+use App\Http\Livewire\Goals;
+use App\Models\Goal;
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Livewire\livewire;
 
-class DestroyTest extends TestCase
-{
-    /** @test */
-    public function the_component_can_render()
-    {
-        $component = Livewire::test(Destroy::class);
+beforeEach(function () {
 
-        $component->assertStatus(200);
-    }
-}
+    $this->user = User::factory()->create();
+
+    $this->goal = Goal::factory()->create();
+
+    actingAs($this->user);
+
+});
+
+it('should be able to delete a goal', function () {
+
+    $lw = livewire(Goals\Destroy::class, ['goal' => $this->goal])
+        ->call('save');
+
+    $lw->assertHasNoErrors()
+        ->assertEmitted('goal::deleted');
+
+    $this->assertDatabaseMissing('goals', [
+        'id' => $this->goal->id,
+    ]);
+
+});
+
+todo('should be able to disable a goals if that has entries', function () {
+
+});
+
+todo('should be able to disable a goals if that has withdrawals', function () {
+
+});
