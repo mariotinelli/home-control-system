853f76e11f506a8754bcf30324e050b98eb98f7d
mariotinelli <maario_tinelli@hotmail.com>
2023-04-22 13:25:54 -0300

    back: feat - update goal entry and your tests

--- app/Http/Livewire/Goals/Entries/Update.php
+++ app/Http/Livewire/Goals/Entries/Update.php
@@ -2,11 +2,38 @@
 
 namespace App\Http\Livewire\Goals\Entries;
 
+use App\Models\Goal;
+use App\Models\GoalEntry;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Update extends Component
 {
-    public function render()
+
+    public ?Goal $goal = null;
+
+    public ?GoalEntry $goalEntry = null;
+
+    public function rules(): array
+    {
+        return [
+            'goalEntry.amount' => ['required', 'numeric', 'min:1', 'max:1000'],
+            'goalEntry.date' => ['required', 'date'],
+        ];
+    }
+
+    public function save(): void
+    {
+
+        $this->validate();
+
+        $this->goalEntry->save();
+
+        $this->emit('goal::entry::updated');
+
+    }
+
+    public function render(): View
     {
         return view('livewire.goals.entries.update');
     }
--- tests/Feature/Livewire/Goals/Entries/UpdateTest.php
+++ tests/Feature/Livewire/Goals/Entries/UpdateTest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Tests\Feature\Livewire\Investments\Entries;
+namespace Tests\Feature\Livewire\Goals\Entries;
 
-use App\Http\Livewire\Investments;
-use App\Models\Investment;
-use App\Models\InvestmentEntry;
+use App\Http\Livewire\Goals;
+use App\Models\Goal;
+use App\Models\GoalEntry;
 use App\Models\User;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\assertDatabaseCount;
@@ -15,10 +15,10 @@ beforeEach(function () {
 
     $this->user = User::factory()->create();
 
-    $this->investment = Investment::factory()->create();
+    $this->goal = Goal::factory()->create();
 
-    $this->investmentEntry = InvestmentEntry::factory()->create([
-        'investment_id' => $this->investment->id,
+    $this->goalEntry = GoalEntry::factory()->create([
+        'goal_id' => $this->goal->id,
         'amount' => 100,
     ]);
 
@@ -26,25 +26,25 @@ beforeEach(function () {
 
 });
 
-it('should be to update an investment entry', function () {
+it('should be to update an goal entry', function () {
 
-    assertDatabaseCount('investment_entries', 1);
+    assertDatabaseCount('goal_entries', 1);
 
     // Act
-    $lw = livewire(Investments\Entries\Update::class, [
-        'investment' => $this->investment,
-        'investmentEntry' => $this->investmentEntry
+    $lw = livewire(Goals\Entries\Update::class, [
+        'goal' => $this->goal,
+        'goalEntry' => $this->goalEntry
     ])
-        ->set('investmentEntry.amount', 200)
-        ->set('investmentEntry.date', '2021-01-10')
+        ->set('goalEntry.amount', 200)
+        ->set('goalEntry.date', '2021-01-10')
         ->call('save');
 
     // Assert
     $lw->assertHasNoErrors()
-        ->assertEmitted('investment::entry::updated');
+        ->assertEmitted('goal::entry::updated');
 
-    assertDatabaseHas('investment_entries', [
-        'investment_id' => $this->investment->id,
+    assertDatabaseHas('goal_entries', [
+        'goal_id' => $this->goal->id,
         'amount' => 200,
         'date' => '2021-01-10',
     ]);
@@ -54,89 +54,89 @@ it('should be to update an investment entry', function () {
 test('amount is required', function () {
 
     // Act
-    $lw = livewire(Investments\Entries\Update::class, [
-        'investment' => $this->investment,
-        'investmentEntry' => $this->investmentEntry
+    $lw = livewire(Goals\Entries\Update::class, [
+        'goal' => $this->goal,
+        'goalEntry' => $this->goalEntry
     ])
-        ->set('investmentEntry.amount', null)
+        ->set('goalEntry.amount', null)
         ->call('save');
 
     // Assert
-    $lw->assertHasErrors(['investmentEntry.amount' => 'required']);
+    $lw->assertHasErrors(['goalEntry.amount' => 'required']);
 
 });
 
 test('amount is numeric', function () {
 
     // Act
-    $lw = livewire(Investments\Entries\Update::class, [
-        'investment' => $this->investment,
-        'investmentEntry' => $this->investmentEntry
+    $lw = livewire(Goals\Entries\Update::class, [
+        'goal' => $this->goal,
+        'goalEntry' => $this->goalEntry
     ])
-        ->set('investmentEntry.amount', 'abc')
+        ->set('goalEntry.amount', 'abc')
         ->call('save');
 
     // Assert
-    $lw->assertHasErrors(['investmentEntry.amount' => 'numeric']);
+    $lw->assertHasErrors(['goalEntry.amount' => 'numeric']);
 
 });
 
 test('amount is min 1', function () {
 
     // Act
-    $lw = livewire(Investments\Entries\Update::class, [
-        'investment' => $this->investment,
-        'investmentEntry' => $this->investmentEntry
+    $lw = livewire(Goals\Entries\Update::class, [
+        'goal' => $this->goal,
+        'goalEntry' => $this->goalEntry
     ])
-        ->set('investmentEntry.amount', 0)
+        ->set('goalEntry.amount', 0)
         ->call('save');
 
     // Assert
-    $lw->assertHasErrors(['investmentEntry.amount' => 'min']);
+    $lw->assertHasErrors(['goalEntry.amount' => 'min']);
 
 });
 
 test('amount is max 1000', function () {
 
     // Act
-    $lw = livewire(Investments\Entries\Update::class, [
-        'investment' => $this->investment,
-        'investmentEntry' => $this->investmentEntry
+    $lw = livewire(Goals\Entries\Update::class, [
+        'goal' => $this->goal,
+        'goalEntry' => $this->goalEntry
     ])
-        ->set('investmentEntry.amount', 1001)
+        ->set('goalEntry.amount', 1001)
         ->call('save');
 
     // Assert
-    $lw->assertHasErrors(['investmentEntry.amount' => 'max']);
+    $lw->assertHasErrors(['goalEntry.amount' => 'max']);
 
 });
 
 test('date is required', function () {
 
     // Act
-    $lw = livewire(Investments\Entries\Update::class, [
-        'investment' => $this->investment,
-        'investmentEntry' => $this->investmentEntry
+    $lw = livewire(Goals\Entries\Update::class, [
+        'goal' => $this->goal,
+        'goalEntry' => $this->goalEntry
     ])
-        ->set('investmentEntry.date', null)
+        ->set('goalEntry.date', null)
         ->call('save');
 
     // Assert
-    $lw->assertHasErrors(['investmentEntry.date' => 'required']);
+    $lw->assertHasErrors(['goalEntry.date' => 'required']);
 
 });
 
 test('date is date', function () {
 
     // Act
-    $lw = livewire(Investments\Entries\Update::class, [
-        'investment' => $this->investment,
-        'investmentEntry' => $this->investmentEntry
+    $lw = livewire(Goals\Entries\Update::class, [
+        'goal' => $this->goal,
+        'goalEntry' => $this->goalEntry
     ])
-        ->set('investmentEntry.date', 'abc')
+        ->set('goalEntry.date', 'abc')
         ->call('save');
 
     // Assert
-    $lw->assertHasErrors(['investmentEntry.date' => 'date']);
+    $lw->assertHasErrors(['goalEntry.date' => 'date']);
 
 });
