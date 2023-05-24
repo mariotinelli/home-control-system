ec5e40fce393aed4be77f05d5a7002a7ce932c65
mariotinelli <maario_tinelli@hotmail.com>
2023-05-24 10:01:36 -0300

    HCS-30: couple spending: test can render spending description column in table

--- tests/Feature/Livewire/Couple/Spending/IndexTest.php
+++ tests/Feature/Livewire/Couple/Spending/IndexTest.php
@@ -55,6 +55,15 @@ it('can render spending id column in table', function () {
 
 })->group('canRenderTableColumn');
 
+it('can render spending description column in table', function () {
+
+    CoupleSpending::factory()->count(1)->create();
+
+    livewire(Couple\Spending\Index::class)
+        ->assertCanRenderTableColumn('description');
+
+})->group('canRenderTableColumn');
+
 it('cannot render page if not has permission', function () {
     $this->user->revokePermissionTo('couple_spending_read');
 
