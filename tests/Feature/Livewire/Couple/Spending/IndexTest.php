f5bdc388aa2997309435a6f79cc5db803da78d41
mariotinelli <maario_tinelli@hotmail.com>
2023-05-24 10:33:35 -0300

    HCS-30: couple spending: test can sort spending by description

--- tests/Feature/Livewire/Couple/Spending/IndexTest.php
+++ tests/Feature/Livewire/Couple/Spending/IndexTest.php
@@ -136,6 +136,7 @@ it('spending are sorted by default in desc order', function () {
 })->group('default');
 
 it('can sort spending by id', function () {
+
     // Arrange
     $spending = CoupleSpending::factory()->count(10)->create([
         'user_id'                     => $this->user->id,
@@ -147,6 +148,36 @@ it('can sort spending by id', function () {
         ->assertCanSeeTableRecords($spending->sortBy('id'), inOrder: true)
         ->sortTable('id', 'desc')
         ->assertCanSeeTableRecords($spending->sortByDesc('id'), inOrder: true);
+
+})->group('canSortTable');
+
+todo('can sort spending by category', function () {
+    // Arrange
+    $spending = CoupleSpending::factory()->count(10)->create([
+        'user_id'                     => $this->user->id,
+        'couple_spending_category_id' => $this->category->id,
+    ]);
+
+    livewire(Couple\Spending\Index::class)
+        ->sortTable('category')
+        ->assertCanSeeTableRecords($spending->sortBy('category'), inOrder: true)
+        ->sortTable('category', 'desc')
+        ->assertCanSeeTableRecords($spending->sortByDesc('category'), inOrder: true);
+
+})->group('canSortTable');
+
+it('can sort spending by description', function () {
+    // Arrange
+    $spending = CoupleSpending::factory()->count(10)->create([
+        'user_id'                     => $this->user->id,
+        'couple_spending_category_id' => $this->category->id,
+    ]);
+
+    livewire(Couple\Spending\Index::class)
+        ->sortTable('description')
+        ->assertCanSeeTableRecords($spending->sortBy('description'), inOrder: true)
+        ->sortTable('description', 'desc')
+        ->assertCanSeeTableRecords($spending->sortByDesc('description'), inOrder: true);
 
 })->group('canSortTable');
 
