915f7d4850f4bfec34894c2ba7a03d8bf4702614
mariotinelli <maario_tinelli@hotmail.com>
2023-05-25 20:00:26 -0300

    HCS-40: check: melhorar dropdown do usuário para um tamanho maior

--- resources/views/components/app/navbar/dropdown/content.blade.php
+++ resources/views/components/app/navbar/dropdown/content.blade.php
@@ -1,5 +1,9 @@
 <div
-    class="absolute right-0 z-50 mt-2 w-fit p-3 divide-y origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+    class="
+        absolute right-0 z-50 mt-2 w-64 max-w-xs p-3 divide-y origin-top-right
+        rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5
+        focus:outline-none
+    "
     role="menu"
     aria-orientation="vertical"
     aria-labelledby="user-menu-button"
